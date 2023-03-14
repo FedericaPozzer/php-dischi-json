ae95a1efbaa813a5aa10b58f617ef70b8e356cdb
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-14 18:31:51 +0100

    done *.*

---
+++ script.js
@@ -0,0 +1,25 @@
+// con vue richiama la API (axios get) e poi stampala col solito v-for
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            dischi: [],
+        }
+    },
+
+    mounted() {
+        axios
+        .get("./script.php")
+        .then((response) => {
+            this.dischi = response.data;
+        })
+
+    }
+
+})
+app.mount("#myApp");
+
+   
+
+   
